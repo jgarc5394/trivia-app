f641dd0d4deeba35d92139fe041ade767a527353
Julio Garcia <jgarc5394@gmail.com>
2023-10-15 16:01:21 -0500

    refactor a bit

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+ import MainScore from '@/components/MainScore.vue'
+</script>
+
+<template>
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main> 
+  <MainScore />
+</template>
+
+<style lang="postcss" scoped>
+  main {
+    @apply flex h-screen items-center justify-center bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-red-200 to-red-600;
+    & .card {
+      @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+    }
+  }
+</style>
