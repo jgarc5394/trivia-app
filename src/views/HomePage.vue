f2beaf8a26d6449d404e0ce79ecba62ac3a52cbb
Julio Garcia <jgarc5394@gmail.com>
2023-10-15 15:11:37 -0500

    Main page layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,5 +2,40 @@
 </script>
 
 <template>
-  <h1 class="text-6xl font-thin text-slate-800">I'm Home Page</h1>
+  <div class="brand">
+    <img class="logo" src="logo.svg" alt="logo" />
+    <h1 class="title">Trivia App</h1>
+    <img class="logo" src="logo.svg" alt="logo" />
+  </div>
+  <div class="categories">
+    <RouterLink v-for="n in 24" :key="n" :to="`/question/category/${n}`" class="category">
+      Category - {{ n }}
+    </RouterLink>
+  </div>
 </template>
+
+<style lang="postcss" scoped>
+.brand {
+  @apply flex items-center justify-center gap-4;
+
+  & .logo {
+    @apply h-16 w-16;
+  }
+
+  & .title {
+    @apply text-6xl font-bold uppercase tracking-widest text-slate-700;
+  }
+}
+
+.categories {
+  @apply grid flex-grow grid-cols-4 gap-12;
+
+  & .category {
+    @apply flex h-32 items-center justify-center rounded-lg border-4 border-pink-400 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
+
+    &:hover {
+      @apply cursor-pointer border-red-500 bg-red-500 text-white;
+    }
+  }
+}
+</style>
